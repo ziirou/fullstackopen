0cc2ea6b44657ffc0c45888195c8b9471658c7f4
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-21 14:59:45 +0200

    Exercise 3.4

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -43,9 +43,22 @@ app.get('/api/persons', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
   const id = request.params.id
   const person = persons.find(person => person.id === id)
-  
+
   if (person) {
     response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
   } else {
     response.status(404).end()
   }
