fe40cfb30d041a0882959baa8cea9383e51dd593
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-21 15:12:49 +0200

    Exercise 3.5

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     id: "1",
@@ -49,6 +51,25 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+const generateId = () => {
+  const randomId = Math.floor(Math.random() * 100)
+  return String(randomId)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
