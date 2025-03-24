aaa4eaf8dd51af409ecafdbafcb1c9415a9df08d
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-24 13:39:27 +0200

    Remove CORS

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-content'))
 app.use(express.static('dist'))
