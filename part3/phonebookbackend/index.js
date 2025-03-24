931c91a9f57f485da7d83620e5cd80e696aac01b
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-24 12:31:43 +0200

    Exercise 3.11 - serve static files

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-content'))
+app.use(express.static('dist'))
 
 morgan.token('post-content', (request) =>
   request.method === 'POST' && request.body
