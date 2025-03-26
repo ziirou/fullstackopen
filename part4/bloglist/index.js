7a9d6d6cd5dc75aefd3821fbf670f901a895a8f4
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-26 09:06:13 +0200

    Exercise 4.1 - modify toJSON output

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -11,6 +11,14 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 })
 
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
