b7c457a62d8a4a5e8b3542c21e6c308dd643152f
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-26 09:23:55 +0200

    Exercise 4.2 - separate blogs router

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
 const mongoUrl = process.env.MONGODB_URI
@@ -11,23 +11,7 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
