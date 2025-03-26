1cfe2800189a6d25376764534698ae66bab9c188
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-26 11:43:32 +0200

    Exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,19 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog_object) => {
+    const likes = blog_object.likes
+    return sum + likes
+  }
+
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
