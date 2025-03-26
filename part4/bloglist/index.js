a5fddb184e4be89ece4bdcdcad082e9b21cfbc16
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-26 09:18:27 +0200

    Exercise 4.2 - separate blog model

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -2,24 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Blog = require('./models/blog')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
