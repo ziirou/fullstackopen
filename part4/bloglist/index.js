d6bc6a05fad2ff8e00282f3146d13957cae1e91b
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-26 09:33:43 +0200

    Exercise 4.2 - separate config

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,19 +1,17 @@
-require('dotenv').config()
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
