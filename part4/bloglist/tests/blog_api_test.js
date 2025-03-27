234d39ca555d891adff1061abc89dcb48416acd5
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-27 11:08:47 +0200

    Exercise 4.8

---
+++ part4/bloglist/tests/blog_api_test.js
@@ -0,0 +1,32 @@
+const { test, describe, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const testHelper = require('./test_helper')
+const Blog = require('../models/blog')
+
+describe('when there is initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(testHelper.initialBlogs)
+  })
+
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, testHelper.initialBlogs.length)
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
---
+++ part4/bloglist/tests/test_helper.js
@@ -0,0 +1,24 @@
+const initialBlogs = [
+  {
+    title: 'First Blog',
+    author: 'First Person',
+    url: 'first_url',
+    likes: 1
+  },
+  {
+    title: 'Second Blog',
+    author: 'Second Person',
+    url: 'second_url',
+    likes: 20
+  },
+  {
+    title: 'Third Blog',
+    author: 'Third Person',
+    url: 'third_url',
+    likes: 300
+  }
+]
+
+module.exports = {
+  initialBlogs
+}
