0f519a677124cd87b2178e9ed49a56431a0d445a
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-31 13:01:46 +0300

    Exercise 4.21

--- part4/README.md
+++ part4/README.md
@@ -1,3 +1,3 @@
-# Exercises 4.1-4.20
+# Exercises 4.1-4.21
 
 The answers for the exercises of part 4
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -36,8 +36,20 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
-  response.status(204).end()
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+  const blog = await Blog.findById(request.params.id)
+
+  if (blog.user.toString() === user._id.toString()) {
+    await Blog.deleteOne(blog)
+    response.status(204).end()
+  } else {
+    return response.status(401).json({ error: 'unauthorized user' })
+  }
 })
 
 blogsRouter.put('/:id', async (request, response) => {
