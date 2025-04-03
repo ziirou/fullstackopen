ab2eaecf3dff640d2f6d5213af79b9563d463a28
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-04-03 14:36:27 +0300

    Exercise 5.14

--- part5/README.md
+++ part5/README.md
@@ -1,3 +1,3 @@
-# Exercises 5.1-5.13
+# Exercises 5.1-5.14
 
 The answers for the exercises of part 5
--- part5/bloglist-frontend/src/tests/Blog.test.jsx
+++ part5/bloglist-frontend/src/tests/Blog.test.jsx
@@ -1,18 +1,44 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from '../components/Blog'
 
 describe('<Blog />', () => {
-  test('renders content', () => {
+  test('Renders blog content', () => {
     const blog = {
       title: 'test_blog',
-      author: 'tester'
+      author: 'test_author',
     }
 
     const { container } = render(<Blog blog={blog} />)
 
     const div = container.querySelector('.blog')
-    expect(div).toHaveTextContent('test_blog - tester')
+    expect(div).toHaveTextContent('test_blog - test_author')
     expect(div).not.toHaveTextContent('URL:')
     expect(div).not.toHaveTextContent('Likes:')
+  })
+
+  test('After clicking the button, details are displayed', async () => {
+    const blog = {
+      title: 'test_blog',
+      author: 'test_author',
+      url: 'test_url',
+      likes: 100,
+      user: {
+        username: 'test_user',
+        name: 'Test User'
+      },
+    }
+
+    const { container } = render(<Blog blog={blog} />)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('View')
+    await user.click(button)
+
+    const div = container.querySelector('.blog')
+    expect(div).toHaveTextContent('test_blog - test_author')
+    expect(div).toHaveTextContent('URL: test_url')
+    expect(div).toHaveTextContent('Likes: 100')
+    expect(div).toHaveTextContent('User: Test User')
   })
 })
