3f6b3179898d1a904ed5fb92fd39a8a54be6e129
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-04-08 11:09:48 +0300

    Exercise 6.11

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from '@reduxjs/toolkit'
+
 const anecdotesAtStart = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -17,31 +19,24 @@ const asObject = (anecdote) => {
   }
 }
 
-export const createAnecdote = (content) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    payload: asObject(content)
-  }
-}
-
-export const voteAnecdote = (id) => {
-  return {
-    type: 'VOTE',
-    payload: { id }
-  }
-}
-
 const initialState = anecdotesAtStart.map(asObject)
 
-const anecdoteReducer = (state = initialState, action) => {
-  console.log('STATE: ', state)
-  console.log('ACTION: ', action)
+const anecdoteSlice = createSlice({
+  name: 'anecdotes',
+  initialState,
+  reducers: {
+    createAnecdote(state, action) {
+      console.log('STATE:', JSON.parse(JSON.stringify(state)))
+      console.log('ACTION:', JSON.parse(JSON.stringify(action)))
 
-  switch(action.type) {
-    case 'NEW_ANECDOTE':
-      return [...state, action.payload]
-    case 'VOTE': {
-      const id = action.payload.id
+      const content = action.payload
+      state.push(asObject(content))
+    },
+    voteAnecdote(state, action) {
+      console.log('STATE:', JSON.parse(JSON.stringify(state)))
+      console.log('ACTION:', JSON.parse(JSON.stringify(action)))
+
+      const id = action.payload
       const anecdoteToChange = state.find(a => a.id === id)
       const changedAnecdote = {
         ...anecdoteToChange,
@@ -53,9 +48,8 @@ const anecdoteReducer = (state = initialState, action) => {
         )
         .sort((less, more) => more.votes - less.votes)
     }
-    default:
-      return state
   }
-}
+})
 
-export default anecdoteReducer
+export const { createAnecdote, voteAnecdote } = anecdoteSlice.actions
+export default anecdoteSlice.reducer
