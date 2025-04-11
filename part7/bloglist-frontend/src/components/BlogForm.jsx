ae38c5cef28f2fdf6b7ab8173e3dc8340a8c118a
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-04-11 11:47:37 +0300

    Copy bloglist-frontend from part4

---
+++ part7/bloglist-frontend/src/components/BlogForm.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+
+const BlogForm = ({ handleBlogCreate }) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    handleBlogCreate({
+      title: title,
+      author: author,
+      url: url,
+    })
+
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  }
+
+  return (
+    <div>
+      <h2>Create new blog</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Title:
+          <input
+            type="text"
+            value={title}
+            aria-label="Title"
+            onChange={event => setTitle(event.target.value)}
+          />
+        </div>
+        <div>
+          Author:
+          <input
+            type="text"
+            value={author}
+            aria-label="Author"
+            onChange={event => setAuthor(event.target.value)}
+          />
+        </div>
+        <div>
+          URL:
+          <input
+            type="text"
+            value={url}
+            aria-label="URL"
+            onChange={event => setUrl(event.target.value)}
+          />
+        </div>
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  )
+}
+
+BlogForm.propTypes = {
+  handleBlogCreate: PropTypes.func.isRequired
+}
+
+export default BlogForm
