86b66cbe6279d53ebd2ecd54d7cfa41a1ab5ded9
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-04-24 11:23:27 +0300

    Exercise 8.1

--- part8/README.md
+++ part8/README.md
@@ -1,3 +1,3 @@
-# Exercises 8
+# Exercises 8.1-8.1
 
 The answers for the exercises of part 8
--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -93,19 +93,17 @@ let books = [
   },
 ]
 
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
-
 const typeDefs = `
   type Query {
-    dummy: Int
+    bookCount: Int!
+    authorCount: Int!
   }
 `
 
 const resolvers = {
   Query: {
-    dummy: () => 0
+    bookCount: () => books.length,
+    authorCount: () => authors.length
   }
 }
 
