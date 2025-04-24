ad8960c27caf89dbf62b59db6855ae5ea9e391cc
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-04-24 12:01:35 +0300

    Exercise 8.4

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -112,7 +112,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -121,7 +121,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.author) {
+        return books.filter((book) => book.author === args.author)
+      }
+      return books
+    },
     allAuthors: () =>
       authors.map((author) => ({
         ...author,
