d717811c6549791e88dccb1735405ffb1eb9c77c
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-04-24 12:11:35 +0300

    Exercise 8.5

--- part8/README.md
+++ part8/README.md
@@ -1,3 +1,3 @@
-# Exercises 8.1-8.4
+# Exercises 8.1-8.5
 
 The answers for the exercises of part 8
--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -112,7 +112,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -122,8 +122,16 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (args.author) {
+      if (args.author && args.genre) {
+        return books.filter(
+          (book) =>
+            book.author === args.author &&
+            book.genres.find((genre) => genre === args.genre)
+        )
+      } else if (args.author) {
         return books.filter((book) => book.author === args.author)
+      } else if (args.genre) {
+        return books.filter((book) => book.genres.find((genre) => genre === args.genre))
       }
       return books
     },
