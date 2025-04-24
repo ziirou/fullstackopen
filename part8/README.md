1e53034be4961143da66efdbd9a64583fd341566
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-04-24 12:28:08 +0300

    Exercise 8.6

--- part8/README.md
+++ part8/README.md
@@ -1,3 +1,3 @@
-# Exercises 8.1-8.5
+# Exercises 8.1-8.6
 
 The answers for the exercises of part 8
--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -96,17 +97,17 @@ let books = [
 const typeDefs = `
   type Book {
     title: String!
-    published: Int!
     author: String!
-    id: ID!
+    published: Int!
     genres: [String!]!
+    id: ID!
   }
 
   type Author {
     name: String!
-    id: ID!
     born: Int
     bookCount: Int!
+    id: ID!
   }
 
   type Query {
@@ -114,6 +115,15 @@ const typeDefs = `
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book!
   }
 `
 
@@ -140,6 +150,19 @@ const resolvers = {
         ...author,
         bookCount: books.filter((book) => book.author === author.name).length,
       })),
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const bookAuthor = authors.find((author) => author.name === args.author)
+      if (!bookAuthor) {
+        const newAuthor = { name: args.author, id: uuid() }
+        authors = authors.concat(newAuthor)
+      }
+
+      const newBook = { ...args, id: uuid() }
+      books = books.concat(newBook)
+      return newBook
+    }
   }
 }
 
