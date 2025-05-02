155a21fd939a92061771c9626959cc59b387205c
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-05-02 14:56:18 +0300

    Exercise 9.4

--- part9/calculator/index.ts
+++ part9/calculator/index.ts
@@ -1,1 +1,12 @@
-console.log('index.ts');
+import express from 'express';
+const app = express();
+
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!');
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
